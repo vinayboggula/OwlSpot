d44cc2006d1c91f8291d9da6f7812eb9723173f6
Vinay B <boggulavinay26@gmail.com>
2026-07-15 16:19:04 +0530

    import missing

--- server/utils/s3SignedUrl.js
+++ server/utils/s3SignedUrl.js
@@ -5,6 +5,13 @@ import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const generateSignedUrl = async (key) => {
+    if (!key) return null;
+
+    // Already an external URL (Unsplash, ImageKit, Picsum)
+    if (key.startsWith("http://") || key.startsWith("https://")) {
+        return key;
+    }
+
     const command = new GetObjectCommand({
         Bucket: process.env.AWS_BUCKET_NAME,
         Key: key,
